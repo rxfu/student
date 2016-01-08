19c194fd0e3ed8387777f329a93ec076a9db78c4
Stephen Foo <stephen.frx@gmail.com>
2016-01-09 00:07:13 +0800

    add user table and ignore remember token

--- app/User.php
+++ app/User.php
@@ -4,23 +4,59 @@ namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
-{
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+class User extends Authenticatable {
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'xh', 'mm', 'zpzt',
+	];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'mm', 'zpzt',
+	];
+
+	protected $table = 'xk_xsmm';
+
+	public $incrementing = false;
+
+	public $timestamps = false;
+
+	protected $primaryKey = 'xh';
+
+	public function getAuthIdentifierName() {
+		return $this->xh;
+	}
+
+	public function getAuthPassword() {
+		return $this->mm;
+	}
+
+	public function getRememberToken() {
+		return null;
+	}
+
+	public function setRememberToken($value) {
+
+	}
+
+	public function getRememberTokenName() {
+		return null;
+	}
+
+	/**
+	 * 覆盖原方法，忽略remember token
+	 */
+	public function setAttribute($key, $value) {
+		if ($key != $this->getRememberTokenName()) {
+			parent::setAttribute($key, $value);
+		}
+	}
 }
--- config/database.php
+++ config/database.php
@@ -71,7 +71,7 @@ return [
 			'username' => env('DB_USERNAME', 'forge'),
 			'password' => env('DB_PASSWORD', ''),
 			'charset'  => 'utf8',
-			'prefix'   => '',
+			'prefix'   => 't_',
 			'schema'   => 'public',
 		],
 
