c2f76e93ad917993864f341318c07b659b92a93a
Stephen <stephen.frx@gmail.com>
2016-02-23 11:13:43 +0800

    fixed exam contrller

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -106,35 +106,6 @@ class ExamController extends Controller {
 	}
 
 	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function create() {
-		//
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function store(Request $request) {
-		//
-	}
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function show($id) {
-		//
-	}
-
-	/**
 	 * 显示学生报名表
 	 * @author FuRongxin
 	 * @date    2016-02-22
