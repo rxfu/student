71f680ba4a1810e8d3d828a935ab3c74a21092cc
Stephen <stephen.frx@gmail.com>
2016-03-24 16:45:53 +0800

    add BaseModel comments

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -5,6 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 删除模型所用的Model基类
+ *
+ * @author FuRongxin
+ * @date 2016-03-24
+ * @version 2.0
+ */
 class BaseModel extends Model {
 
 	/**
