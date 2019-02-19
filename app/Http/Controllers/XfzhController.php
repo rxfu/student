8f4e2ba0442de5de095b8af9c32ce95436ae32f1
Stephen Fu <stephen.frx@gmail.com>
2019-02-19 16:18:41 +0800

    fixed xfzh view

--- app/Http/Controllers/XfzhController.php
+++ app/Http/Controllers/XfzhController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Plan;
+use App\Models\Score;
 use App\Models\Xfzhsq;
 use Auth;
 use Illuminate\Http\Request;
@@ -18,14 +19,32 @@ class XfzhController extends Controller {
 	}
 
 	public function create() {
-		$courses = Plan::with('course')
+		$studied_courses = Score::with('course', 'platform', 'property')
+			->whereXh(Auth::user()->xh)
+			->where('xf', '<>', 0)
+			->whereNotIn('kch', function ($query) {
+				$query->from('xk_xfzhkc')
+					->join('xk_xfzhsq', 'appid', '=', 'xk_xfzhkc.id')
+					->whereXh(Auth::user()->xh)
+					->whereZt(4)
+					->select('qkch');
+			})
+			->get();
+
+		$courses = Plan::with('course', 'platform', 'property')
 			->whereZy(Auth::user()->profile->zy)
 			->whereNj(Auth::user()->profile->nj)
 			->whereZsjj(Auth::user()->profile->zsjj)
+			->WhereNotIn('kch', function ($query) {
+				$query->from('cj_zxscj')
+					->whereXh(Auth::user()->xh)
+					->where('xf', '>', 0)
+					->select('kch');
+			})
 			->get();
 		$title = "学分转换申请";
 
-		return view('xfzh.create', compact('courses', 'title'));
+		return view('xfzh.create', compact('studied_courses', 'courses', 'title'));
 	}
 
 	public function store(Request $request) {
